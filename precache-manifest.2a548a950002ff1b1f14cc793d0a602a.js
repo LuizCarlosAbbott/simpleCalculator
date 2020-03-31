a658f27fe977cd605f556a78c0c70da4e0d59fea
@LuizCarlosAbbott <luizcarlos_abbott@hotmail.com>
2020-03-30 22:53:08 -0300

    Updates

---
+++ precache-manifest.2a548a950002ff1b1f14cc793d0a602a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbbb1d7bf7c084d0dc47d57316b7700",
+    "url": "/simpleCalculator/index.html"
+  },
+  {
+    "revision": "07c81dc5358548d2eeec",
+    "url": "/simpleCalculator/static/css/main.02ac42fa.chunk.css"
+  },
+  {
+    "revision": "1646823336a7f1f40224",
+    "url": "/simpleCalculator/static/js/2.bb80dcb8.chunk.js"
+  },
+  {
+    "revision": "07c81dc5358548d2eeec",
+    "url": "/simpleCalculator/static/js/main.4566df80.chunk.js"
+  },
+  {
+    "revision": "54cab9c956454300a017",
+    "url": "/simpleCalculator/static/js/runtime~main.93b3baed.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/simpleCalculator/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
